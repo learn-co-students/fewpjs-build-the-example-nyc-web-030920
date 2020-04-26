4334988d6e676329d2c2ffd86152a1f6f3b13457
JohnsonNYC <jkow95@gmail.com>
2020-04-26 15:44:30 -0400

    Done.

--- main.js
+++ main.js
@@ -3,22 +3,60 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById('modal') // Error Node
+const heartBtn = document.querySelectorAll('span.like-glyph') //heart buttons span elements
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+document.addEventListener('DOMContentLoaded', event => {
+  errorModal.hidden = true
+  heartClick() // Turn Heat Red On Click
+})
+
+function heartClick() {// forEach span, if clicked invoke function
+    heartBtn.forEach(span => {
+    span.addEventListener('click', callback);
+  })
+}
+
+function callback(e){
+  let heart = e.target
+  mimicServerCall()
+  .then(message => { // DO THIS WHEN PROMISE IS FULFILLED 
+    heart.innerText = glyphStates[heart.innerText]
+    heart.style.color = colorStates[heart.style.color]
+    console.log('Promise Fulfilled')
+  })
+  .catch(message => { // DO THIS WHEN PROMISE IS REJECTED 
+    errorModal.hidden = false 
+    setTimeout(function(){
+      errorModal.hidden = true
+    },5000)
+  })
+
+}
 
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) { //makes server request 
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject("Random server error. Try again."); // REJECTED 
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve("Pretend remote server notified of action!"); // FULFILLED
       }
     }, 300);
   });
