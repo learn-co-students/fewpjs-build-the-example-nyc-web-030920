1c50d78d934de70b928c1db0a5d803a11742dfd5
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-04-28 14:53:13 -0400

    Done.

--- main.js
+++ main.js
@@ -2,7 +2,41 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+
 // Your JavaScript code goes here!
+
+document.addEventListener('DOMContentLoaded', function(){
+  addLikeListener();
+})
+
+const addLikeListener = () => {
+  let allLikeBtn = document.querySelectorAll('.like');
+  allLikeBtn.forEach(likeBtn => {
+    likeBtn.addEventListener('click', (event) => {
+      let heart = event.target;
+      mimicServerCall('idkUrl')
+      .then(response => {
+        console.log(response)
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(error => {
+        console.log(error)
+        document.getElementById("modal").className = "";
+      })
+    })
+  })
+}
 
 
 
